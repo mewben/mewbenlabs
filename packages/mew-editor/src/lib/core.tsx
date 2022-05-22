96dd0001313f312ab825bba6d9ca95d8721b3c72
melvin <melvinsoldia@gmail.com>
2022-05-22 18:09:11 +0800

    initial mew-editor

---
+++ packages/mew-editor/src/lib/core.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Core = () => {
+  return <div className="bg-red-500 rounded p-8">Core edit with tailwind</div>;
+};
+
+export default Core;
